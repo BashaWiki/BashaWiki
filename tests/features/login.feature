2eb34629bcb35b841e1fd13eec057b10cc7ec0b1
BashaWiki <barkathbasha423@gmail.com>
2022-04-08 15:55:30 +0100

    Basic Pytest-BDD framework

---
+++ tests/features/login.feature
@@ -0,0 +1,11 @@
+@testing
+Feature: To validate different types of login
+
+  @testing
+  Scenario: To verify the user login
+    Given The user launched the "Home page"
+    And user validates Home page is launched
+    When user enters "Username" in Username
+    And user enters "Password" in password
+    And user click on login button
+    Then user should be logged into the page
---
+++ tests/step_defs/test_login.py
@@ -0,0 +1,36 @@
+from pytest_bdd import scenario, given, when, then, parsers, scenarios
+
+# @scenario('features\login.feature', 'To verify the user login')
+# def test_to_verify_the_user_login():
+#     pass
+scenarios('../tests/features/')
+
+@given(parsers.parse('The user launched the "{pagename}"'))
+def the_user_launched_the_home_page(pagename):
+    print(pagename)
+
+
+@given('user validates Home page is launched')
+def user_validates_home_page_is_launched():
+    print("Getting Started")
+
+
+@when('user click on login button')
+def user_click_on_login_button():
+    """user click on login button."""
+    pass
+
+
+@when('user enters "Password" in password')
+def user_enters_password_in_password():
+    pass
+
+
+@when('user enters "Username" in Username')
+def user_enters_username_in_username():
+    pass
+
+
+@then('user should be logged into the page')
+def user_should_be_logged_into_the_page():
+    pass
